b3ad23996a8e1cc52139336f78fba04eaa5c1217
Index_532 <66223532@g.cmru.ac.th>
2026-04-07 14:39:01 +0700

    feat: create login page layout

---
+++ src/app/login/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+export default function LoginPage() {
+  const router = useRouter()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      alert('กรอกข้อมูลให้ครบ')
+      return
+    }
+
+    router.push('/home')
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleLogin}>
+        เข้าสู่ระบบ
+      </button>
+    </div>
+  )
+}
