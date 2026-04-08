fde027c11eebee81c5720f577bd2018e5d8dee1b
Index_532 <66223532@g.cmru.ac.th>
2026-04-08 13:39:55 +0700

    feat: create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <div className="hhdr">
+            <div className="hbrand">
+                Garage<em>Plus</em>
+            </div>
+        </div>
+    )
+}
