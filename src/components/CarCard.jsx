a165ce13cb71653d9c7ca6ecb1492209fd080a2b
Index_532 <66223532@g.cmru.ac.th>
2026-04-08 13:43:38 +0700

    feat: create CarCard component

---
+++ src/components/CarCard.jsx
@@ -0,0 +1,8 @@
+export default function CarCard(){
+    return (
+        <div className="hcar">
+            <div className="hcn">Toyota Fortuner</div>
+            <div className="hp1">ชม 3847</div>
+        </div>
+    )
+}
